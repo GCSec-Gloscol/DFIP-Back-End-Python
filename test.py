b799ad8baedd491517556b56ebdc26dbdca0c134
thiago-gloscol <43465794+thiagovianagloscol@users.noreply.github.com>
2018-11-28 09:45:18 +0000

    Create test.py
    Testing.

---
+++ test.py
@@ -0,0 +1,8 @@
+f = open("testfile.txt","a")
+ 
+f.write("Hello World\n")
+f.write("This is our new text file") 
+f.write("and this is another line.") 
+f.write("Why? Because we can.") 
+ 
+f.close()
